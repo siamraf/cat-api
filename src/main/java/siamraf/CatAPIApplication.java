b89118bbc5cdb7e9b9efb21effebeaa88d895bc6
siamraf <siamraf@gmail.com>
2015-11-30 00:00:54 +0000

    Implement main method on CatAPIApplication.java

--- src/main/java/siamraf/CatAPIApplication.java
+++ src/main/java/siamraf/CatAPIApplication.java
@@ -1,9 +1,12 @@
 package siamraf;
 
 import siamraf.category.CategoryProvider;
+import siamraf.category.HttpCatApiCategoryProvider;
 import siamraf.fact.CatFactProvider;
+import siamraf.fact.HttpCatApiFactProvider;
 import siamraf.image.CatImage;
 import siamraf.image.CatImageProvider;
+import siamraf.image.HttpCatApiImageProvider;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +20,10 @@ import java.util.Comparator;
 import java.util.List;
 
 public class CatAPIApplication {
+
+    public static final String CAT_API_CATEGORIES = "http://thecatapi.com/api/categories/list";
+    public static final String CAT_API_FACTS = "http://catfacts-api.appspot.com/api/facts";
+    public static final String CAT_API_IMAGES = "http://thecatapi.com/api/images/get?format=xml";
 
     private final CatFactProvider factProvider;
     private final CatImageProvider imageProvider;
@@ -38,8 +45,34 @@ public class CatAPIApplication {
 
     public static void main(String[] args) {
         if (args.length != 1) {
-
+            printUsage();
+            return;
         }
+
+        CatAPIApplication catAPIApplication = new CatAPIApplication(System.out,
+                new HttpCatApiCategoryProvider(CAT_API_CATEGORIES),
+                new HttpCatApiFactProvider(CAT_API_FACTS),
+                new HttpCatApiImageProvider(CAT_API_IMAGES),
+                new File(System.getProperty("java.io.tmpdir")));
+
+        String command = args[0];
+        switch (command) {
+            case "file":
+                catAPIApplication.saveCatImage();
+                break;
+            case "categories":
+                catAPIApplication.printCategories();
+                break;
+            case "fact":
+                catAPIApplication.printFact();
+                break;
+            default:
+                printUsage();
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage:\n jar CatAPI.jar [ file | categories | fact ]");
     }
 
     public void printCategories() {
@@ -59,10 +92,14 @@ public class CatAPIApplication {
         output.flush();
     }
 
-    public void saveCatImage() throws IOException {
-        CatImage catImage = imageProvider.getCatImage();
-        Path localImagePath = imageDir.toPath().resolve(catImage.getImageName());
-        Files.write(localImagePath, catImage.getImageData(), StandardOpenOption.CREATE);
-        printAndFlush("Image from " + catImage.getImageLocation() + " saved to " + localImagePath.toUri());
+    public void saveCatImage() {
+        try {
+            CatImage catImage = imageProvider.getCatImage();
+            Path localImagePath = imageDir.toPath().resolve(catImage.getImageName());
+            Files.write(localImagePath, catImage.getImageData(), StandardOpenOption.CREATE);
+            printAndFlush("Image from " + catImage.getImageLocation() + " saved to " + localImagePath.toUri());
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing image to file", e);
+        }
     }
 }
